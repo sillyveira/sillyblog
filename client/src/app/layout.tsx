af00950de721ea74167afba103c3ac54211af312
sillyveira <wslc@cin.ufpe.br>
2025-08-07 13:46:15 -0300

    feat: add authContext to globally store user data

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
 import '@ant-design/v5-patch-for-react-19';
+import { AuthProvider } from "@/contexts/authContext";
 //import "antd/dist/reset.css"
 
 export const metadata: Metadata = {
@@ -13,9 +14,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
       <>
         <html>
-          <body className="overflow-hidden pt-[64px]">
+          <body className="h-screen overflow-hidden">
+          <AuthProvider>              
             <Header />
-            {children}
+            <main className="h-full pt-16 overflow-hidden">
+              {children}
+            </main>
+          </AuthProvider>
           </body>
         </html>
       </>
